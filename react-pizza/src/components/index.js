b56b854725bf6960f7cee794cdf1f995c3131067
Artem78780 <a.p.paraschuk@gmail.com>
2021-10-19 21:27:04 +0300

    8/1.19

--- react-pizza/src/components/index.js
+++ react-pizza/src/components/index.js
@@ -2,6 +2,7 @@ import Button from "./button/Button";
 import Header from "./header/Header";
 import Categories from "./categories/Categories";
 import SortPopup from "./sortPopup/SortPopup";
-import PizzaBlock from "./pizzaBlock/PizzaBlock";
+import PizzaBlock from "./pizzaBlock";
+import LoadingBlock from "./pizzaBlock/LoadingBlock";
 
-export {Button, Header, Categories, SortPopup, PizzaBlock}; 
+export {Button, Header, Categories, SortPopup, PizzaBlock, LoadingBlock}; 
--- react-pizza/src/components/pages/Home.jsx
+++ react-pizza/src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux'
 
-import { Categories, SortPopup, PizzaBlock } from "../../components";
+import { Categories, SortPopup, PizzaBlock, LoadingBlock} from "../../components";
 
 import{setCategory} from '../../redux/action/filters'
+import { fetchPizzas } from "../../redux/action/pizzas";
+
 
 const categoryNames = ["Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
 const sortItems = [
@@ -15,13 +17,18 @@ const sortItems = [
 function Home() { 
   const dispatch = useDispatch();
 	const items= useSelector(({pizzas}) => pizzas.items);
+  const isLoaded= useSelector(({pizzas}) => pizzas.isLoaded);
+
+  React.useEffect(() =>{
+    
+		// dispatch(fetchPizzas())
+	}, [])
 
   const onSelectCategory = React.useCallback((index) => {
     dispatch(setCategory(index));
   },[]);
 
-
-    
+  
   return ( 
     <div className="container">
       <div className="content__top">
@@ -30,9 +37,8 @@ function Home() {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {items && items.map((obj) => (
-          <PizzaBlock key={obj.id} {...obj} />
-        ))}
+        {isLoaded && items.map((obj) => <PizzaBlock key={obj.id} isLoading={true} {...obj} />)}
+        {Array(10).fill(<LoadingBlock/>)}
       </div>
     </div>
   ); 
