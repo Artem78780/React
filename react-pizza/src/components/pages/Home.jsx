8bae5a7610e11222d370c6076181696edfc7ea6d
Artem78780 <a.p.paraschuk@gmail.com>
2021-10-21 14:48:28 +0300

    8 finish

--- react-pizza/src/components/pages/Home.jsx
+++ react-pizza/src/components/pages/Home.jsx
@@ -9,9 +9,9 @@ import { fetchPizzas } from "../../redux/action/pizzas";
 
 const categoryNames = ["Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
 const sortItems = [
-  { name: "популярности", type: 'popular' },
-  { name: "цене", type: 'price' },
-  { name: "алфавиту", type: 'alphabet' }
+  { name: "популярности", type: 'popular', order: 'desc' },
+  { name: "цене", type: 'price',  order: 'desc' },
+  { name: "алфавиту", type: 'name',  order: 'asc' },
 ];
 
 function Home() {
@@ -22,7 +22,7 @@ function Home() {
 
   React.useEffect(() => {
 
-    dispatch(fetchPizzas())
+    dispatch(fetchPizzas(sortBy, category))
   }, [category, sortBy])
 
   const onSelectCategory = React.useCallback((index) => {
@@ -41,7 +41,9 @@ function Home() {
           activeCategory={category}
           onClickCategory={onSelectCategory}
           items={categoryNames} />
-        <SortPopup activeSortType={sortBy} items={sortItems} onClickSortType={onSelectSortType}
+        <SortPopup activeSortType={sortBy.type} 
+          items={sortItems} 
+          onClickSortType={onSelectSortType}
         />
       </div>
       <h2 className="content__title">Все пиццы</h2>
