bbd941ec7d69fc889fcde707917f870cbcdd6d3f
Artem78780 <a.p.paraschuk@gmail.com>
2021-10-25 15:59:52 +0300

    1

--- react-pizza/src/components/cartItem/CartItem.jsx
+++ react-pizza/src/components/cartItem/CartItem.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import  Button  from "../button/Button";
 
+const CartItem = ({id, name, type, size, totalPrice, totalCount, onRemove, onPlus, onMinus}) =>{
+    const handleRemoveClick = () =>{
+      onRemove(id)
+    }
 
-const CartItem = ({name, type, size}) =>{
-    return(
+    const handlePluseItems = () =>{
+      onPlus(id);
+    }
+
+    const handleMinusItems = () =>{
+      onMinus(id);
+    }
+
+  return(
         <div className="cart__item">
               <div className="cart__item-img">
                 <img
@@ -16,7 +28,8 @@ const CartItem = ({name, type, size}) =>{
                 <p>{type} тесто, {size} см.</p>
               </div>
               <div className="cart__item-count">
-                <div className="button button--outline button--circle cart__item-count-minus">
+                <div onClick={handleMinusItems}
+                className="button button--outline button--circle cart__item-count-minus">
                   <svg
                     width="10"
                     height="10"
@@ -34,8 +47,9 @@ const CartItem = ({name, type, size}) =>{
                     />
                   </svg>
                 </div>
-                <b>2</b>
-                <div className="button button--outline button--circle cart__item-count-plus">
+                <b>{totalCount}</b>
+                <div onClick={handlePluseItems}
+                className="button button--outline button--circle cart__item-count-plus">
                   <svg
                     width="10"
                     height="10"
@@ -55,10 +69,10 @@ const CartItem = ({name, type, size}) =>{
                 </div>
               </div>
               <div className="cart__item-price">
-                <b>770 ₽</b>
+                <b>{totalPrice} ₴</b>
               </div>
-              <div className="cart__item-remove">
-                <div className="button button--outline button--circle">
+              <div  className="cart__item-remove">
+                <Button onClick={handleRemoveClick} className='button--circle' outline>
                   <svg
                     width="10"
                     height="10"
@@ -75,7 +89,7 @@ const CartItem = ({name, type, size}) =>{
                       fill="#EB5A1E"
                     />
                   </svg>
-                </div>
+                </Button>
               </div>
             </div>
     )
--- react-pizza/src/redux/reducers/cart.js
+++ react-pizza/src/redux/reducers/cart.js
@@ -3,29 +3,91 @@ const initialState = {
     totalPrice: 0,
     totalCount: 0,
 };
-
+const getTotalPrice = arr => arr.reduce((sum, obj) => obj.price + sum, 0);
 const cart = (state = initialState, action) => {
 
-    switch(action.type){
-        case 'ADD_PIZZA_CART':{
+    switch (action.type) {
+        case 'ADD_PIZZA_CART': {
+            const currentPizzaItems = !state.items[action.payload.id]
+                ? [action.payload]
+                : [...state.items[action.payload.id].items, action.payload];
+
             const newItems = {
                 ...state.items,
-                [action.payload.id]: !state.items[action.payload.id] 
-                ? [action.payload] 
-                : [...state.items[action.payload.id], action.payload]
+                [action.payload.id]: {
+                    items: currentPizzaItems,
+                    totalPrice: getTotalPrice(currentPizzaItems),
+                },
             };
+            const totalCount = Object.keys(newItems).reduce(
+                (sum, key) => newItems[key].items.length + sum, 0
+            );
+            const totalPrice = Object.keys(newItems).reduce(
+                (sum, key) => newItems[key].totalPrice + sum, 0
+            );
 
-            const allPizzas = [].concat.apply([], Object.values(newItems));
-            const totalPrice = allPizzas.reduce((sum, obj) => obj.price + sum, 0); 
-
-            return{
+            return {
                 ...state,
                 items: newItems,
-                totalCount: allPizzas.length,
+                totalCount,
                 totalPrice,
             };
         }
-        
+        case 'REMOVE_ART_ITEM':
+            const newItems = {
+                ...state.items
+            }
+            const currentTotalPrice = newItems[action.payload].totalPrice;
+            const currentTotalCount = newItems[action.payload].items.length;
+            delete newItems[action.payload];
+            return {
+                ...state,
+                items: newItems,
+                totalPrice: state.totalPrice - currentTotalPrice,
+                totalCount: state.totalCount - currentTotalCount,
+            }
+
+        case 'PLUS_CART_ITEM':{
+            const newItem = [
+                ...state.items[action.payload].items,
+                state.items[action.payload].items[0]
+            ]
+            return {
+                ...state,
+                items: {
+                    ...state.items,
+                    [action.payload]: {
+                        items: newItem,
+                        totalPrice: getTotalPrice(newItem),
+                    },
+                },
+            }
+        }
+
+        case 'MINUS_CART_ITEM':{
+            const oldItem = state.items[action.payload].items;
+            const newItem = oldItem.length > 1 
+            ? state.items[action.payload].items.slice(1)
+            : oldItem;
+            return {
+                ...state,
+                items: {
+                    ...state.items,
+                    [action.payload]: {
+                        items: newItem,
+                        totalPrice: getTotalPrice(newItem),
+                    },
+                },
+            }
+        }
+
+        case 'CLEAR_CART':
+            return {
+                totalPrice: 0,
+                totalCount: 0,
+                items: {},
+            }
+
         default:
             return state;
     }
